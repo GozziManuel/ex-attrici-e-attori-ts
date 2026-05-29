d0f97923efc0fbb814cff79acc8148241bf1ee0d
Gozzi Manuel <manuel2006gozzi@gmail.com>
2026-05-29 19:22:35 +0200

    milestone 1-2-3-4

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,121 @@
-import './style.css'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.ts'
+//  Milestone 1
+// Crea un type alias Person per rappresentare una persona generica.
+type Person = {
+  // id: numero identificativo, non modificabile
+  readonly id: number;
+  // name: nome completo, stringa non modificabile
+  readonly name: string;
+  // birth_year: anno di nascita, numero
+  birth_year: number;
+  // death_year: anno di morte, numero opzionale
+  death_year?: number;
+  // biography: breve biografia, stringa
+  biography: string;
+  // image: URL dell'immagine, stringa
+  image: string;
+};
+//
+//
+// 📌 Milestone 2
+type Nationality =
+  | "American"
+  | "British"
+  | "Australian"
+  | "Israeli-American"
+  | "South African"
+  | "French"
+  | "Indian"
+  | "Israeli"
+  | "Spanish"
+  | "South Korean"
+  | "Chinese";
+// Crea un type alias Actress che oltre a tutte le proprietà di Person, aggiunge le seguenti proprietà:
+type Actress = Person & {
+  // most_famous_movies: una tuple di 3 stringhe
+  most_famous_movies: [string, string, string];
+  // awards: una stringa
+  awards: string;
+  // nationality: una stringa tra un insieme definito di valori.
+  nationality: Nationality;
+};
+//
+//
+//  Milestone 3
+function isActress(dati: unknown): dati is Actress {
+  if (
+    dati &&
+    typeof dati === "object" &&
+    "id" in dati &&
+    typeof dati.id === "number" &&
+    "name" in dati &&
+    typeof dati.name === "string" &&
+    "birth_year" in dati &&
+    typeof dati.birth_year === "number" &&
+    "death_year" in dati &&
+    typeof dati.death_year === "number" &&
+    "biography" in dati &&
+    typeof dati.biography === "string" &&
+    "image" in dati &&
+    typeof dati.image === "string" &&
+    "most_famous_movies" in dati &&
+    typeof dati.most_famous_movies === "object" &&
+    "awards" in dati &&
+    typeof dati.awards === "string" &&
+    "nationality" in dati &&
+    typeof dati.nationality === "string"
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Crea una funzione getActress che, dato un id, effettua una chiamata a:
+// GET /actresses/:id
+// La funzione deve restituire l’oggetto Actress, se esiste, oppure null se non trovato.
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+// Utilizza un type guard chiamato isActress per assicurarti che la struttura del dato ricevuto sia corretta.
 
-<div class="ticks"></div>
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses/${id}`);
+    const result: unknown = await response.json();
+    console.log(result);
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://www.typescriptlang.org" target="_blank">
-          <img class="button-icon" src="${typescriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+    if (isActress(result)) {
+      return result as Actress;
+    }
+    throw new Error("Tipo non valido");
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error("not a valid URL");
+    }
+    return null;
+  }
+}
+getActress(2);
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+// Milestone 4
+// Crea una funzione getAllActresses che chiama:
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// GET /actresses
+// La funzione deve restituire un array di oggetti Actress.
+
+// Può essere anche un array vuoto.
+async function getAllActresses(): Promise<Actress[]> {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses`);
+    const result: unknown = await response.json();
+    if (!(result instanceof Array)) {
+      throw new Error("not an array");
+    }
+    const dati = result.filter((e) => e);
+    console.log(dati);
+    return dati;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error("");
+    }
+    return [];
+  }
+}
+getAllActresses();
